f16ff2d1f6dd19c3b82ab0804a3b000783937fd7
Dmytro Didukh <jeriko.outback@gmail.com>
2020-07-01 17:51:36 +0300

    socket.io v0.1

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -1,89 +1,102 @@
 import {Request, Response} from "express";
-import { validationResult } from 'express-validator';
+import {validationResult} from 'express-validator';
 import bcrypt from 'bcrypt';
 
-import { UserModel } from '../models';
-import { createJWToken} from '../utils';
+import {UserModel} from '../models';
+import {createJWToken} from '../utils';
 
 class UserController {
-  show(req: Request, res: Response) {
-    const id: string = req.params.id;
-    UserModel.findById(id, (err, user) => {
-      if (err) {
-        return res.status(404).json({
-          message: 'User not found',
+    show(req: Request, res: Response) {
+        const id: string = req.params.id;
+        UserModel.findById(id, (err, user) => {
+            if (err) {
+                return res.status(404).json({
+                    message: 'User not found',
+                });
+            }
+            res.json(user);
         });
-      }
-      res.json(user);
-    });
-  }
-
-  create(req: Request, res: Response) {
-    const postData = {
-      email: req.body.email,
-      fullname: req.body.fullname,
-      password: req.body.password,
-    };
-    const user = new UserModel(postData);
-    user
-      .save()
-      .then((obj: any) => {
-        res.json(obj);
-      })
-      .catch(reason => {
-        res.json(reason);
-      });
-  }
-
-  delete(req: Request, res: Response) {
-    const id: string = req.params.id;
-    UserModel.findOneAndRemove({ _id: id })
-      .then(user => {
-        if (user) {
-          res.json({
-            message: `User ${user.fullname} deleted`,
-          });
-        }
-      })
-      .catch(() => {
-        res.json({
-          message: `User not found`,
-        });
-      });
-  }
-
-  login(req: Request, res: Response) {
-    const postData = {
-      email: req.body.email,
-      password: req.body.password,
-    };
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
     }
 
-    UserModel.findOne({ email: postData.email }, (err, user: any) => {
-      if (err) {
-        return res.status(404).json({
-          message: 'User not found',
+    getMe(req: Request, res: Response) {
+        // @ts-ignore
+        const id: string = req.user._id;
+        UserModel.findById(id, (err, user) => {
+            if (err) {
+                return res.status(404).json({
+                    message: 'User not found',
+                });
+            }
+            res.json(user);
         });
-      }
+    }
 
-      if (bcrypt.compareSync(postData.password, user.password)) {
-        const token = createJWToken(user);
-        res.json({
-          status: 'success',
-          token,
+    create(req: Request, res: Response) {
+        const postData = {
+            email: req.body.email,
+            fullname: req.body.fullname,
+            password: req.body.password,
+        };
+        const user = new UserModel(postData);
+        user
+            .save()
+            .then((obj: any) => {
+                res.json(obj);
+            })
+            .catch(reason => {
+                res.json(reason);
+            });
+    }
+
+    delete(req: Request, res: Response) {
+        const id: string = req.params.id;
+        UserModel.findOneAndRemove({_id: id})
+            .then(user => {
+                if (user) {
+                    res.json({
+                        message: `User ${user.fullname} deleted`,
+                    });
+                }
+            })
+            .catch(() => {
+                res.json({
+                    message: `User not found`,
+                });
+            });
+    }
+
+    login(req: Request, res: Response) {
+        const postData = {
+            email: req.body.email,
+            password: req.body.password,
+        };
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array()});
+        }
+
+        UserModel.findOne({email: postData.email}, (err, user: any) => {
+            if (err) {
+                return res.status(404).json({
+                    message: 'User not found',
+                });
+            }
+
+            if (bcrypt.compareSync(postData.password, user.password)) {
+                const token = createJWToken(user);
+                res.json({
+                    status: 'success',
+                    token,
+                });
+            } else {
+                res.json({
+                    status: 'error',
+                    message: 'Incorrect password or email',
+                });
+            }
         });
-      } else {
-        res.json({
-          status: 'error',
-          message: 'Incorrect password or email',
-        });
-      }
-    });
-  }
+    }
 }
 
 export default UserController;
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import mongoose from 'mongoose';
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import socket from 'socket.io';
+import {createServer} from 'http';
 
 import { UserController, DialogController, MessageController } from './controllers';
-
 import { updateLastSeen, checkAuth } from './middlewares';
 import { loginValidation } from './utils/validations';
 
 const app = express();
+const http = createServer(app);
+const io = socket(http);
+
 dotenv.config();
 
 app.use(bodyParser.json());
@@ -25,6 +29,7 @@ mongoose.connect('mongodb://localhost:27017/chat', {
   useFindAndModify: false,
 });
 
+app.get('/user/me', User.getMe);
 app.get('/user/:id', User.show);
 app.delete('/user/:id', User.delete);
 app.post('/user/registration', User.create);
@@ -38,6 +43,10 @@ app.get('/messages', Messages.index);
 app.post('/messages', Messages.create);
 app.delete('/messages/:id', Messages.delete);
 
-app.listen(process.env.PORT, function() {
+io.on('connection', (socket: any) => {
+  console.log('CONNECTED');
+  socket.emit('test command', 'qwertyQWERTY')
+});
+http.listen(process.env.PORT, () => {
   console.log(`Server: http://localhost:${process.env.PORT}`);
 });
